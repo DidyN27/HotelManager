cc2fb3a1d08e0e7e5158bafa9a1ce5914d6268c7
Eva Valkova <eva.n.valkova@gmail.com>
2026-04-03 11:14:57 +0300

    log in

--- Program.cs
+++ Program.cs
@@ -5,13 +5,10 @@ using Hotel.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// 1. Свързване с базата данни
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlite(connectionString));
 
-// 2. ДОБАВЯНЕ НА IDENTITY (Това липсваше!)
-// Тук казваме на системата да използва ApplicationUser и да добави поддръжка за Razor Pages (Identity страниците)
 builder.Services.AddDefaultIdentity<ApplicationUser>(options => {
     options.SignIn.RequireConfirmedAccount = false;
     options.Password.RequireDigit = false;
@@ -23,11 +20,10 @@ builder.Services.AddDefaultIdentity<ApplicationUser>(options => {
 .AddEntityFrameworkStores<ApplicationDbContext>();
 
 builder.Services.AddControllersWithViews();
-builder.Services.AddRazorPages(); // Нужно е за Identity страниците като Login/Register
+builder.Services.AddRazorPages(); 
 
 var app = builder.Build();
 
-// 3. Конфигурация на Middleware
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
@@ -39,7 +35,6 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
-// ВАЖНО: Първо Authentication (кой си ти), после Authorization (какво можеш да правиш)
 app.UseAuthentication(); 
 app.UseAuthorization();
 
@@ -47,7 +42,20 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
-// Нужно е, за да работят Login/Register страниците
-app.MapRazorPages(); 
+app.MapRazorPages();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        await DbInitializer.SeedAdmin(services);
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred while seeding the database.");
+    }
+}
 
 app.Run();
